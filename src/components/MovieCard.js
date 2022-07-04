69fc54aa7a45ecbc0fb9bcebac1e228e59c2f7c0
ae95medina <ae95medina@gmail.com>
2022-07-04 08:59:32 -0500

    commit tree 6f7cd9019144f7ceace0fae8441b62503d54d071

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,8 @@
 import React from "react";
-
 function MovieCard({
   title,
-  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
+  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
 }) {
   return (
     <div className="movie-card">
@@ -13,5 +12,26 @@ function MovieCard({
     </div>
   );
 }
+
+// //with destructuring
+// function MovieCard({ title, posterSrc, genres }) {
+//   return (
+//     <div className="movie-card">
+//       <img src={posterSrc} alt={title} />
+//       <h2>{title}</h2>
+//       <small>{genres.join(", ")}</small>
+//     </div>
+//   );
+// }
+//without destructuring
+// function MovieCard(props) {
+//   return (
+//     <div className="movie-card">
+//       <img src={props.posterSrc} alt={props.title} />
+//       <h2>{props.title}</h2>
+//       <small>{props.genres.join(", ")}</small>
+//     </div>
+//   );
+// }
 
 export default MovieCard;
---
+++ src/components/SocialMedia.js
@@ -0,0 +1,9 @@
+function SocialMedia({ socialLinks: { github, linkedin } }) {
+    return (
+      <div>
+        <a href={github}>{github}</a>
+        <a href={linkedin}>{linkedin}</a>
+      </div>
+    );
+  }
+  export default SocialMedia;
